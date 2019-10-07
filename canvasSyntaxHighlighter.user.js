ee2046ef7cd05e40c71c9107e806754fe7ef3314
Christopher E <cesbrandt@users.noreply.github.com>
2019-10-07 09:34:28 -0400

    Update canvasSyntaxHighlighter.user.js
    Added support for the new RCE

--- canvasSyntaxHighlighter.user.js
+++ canvasSyntaxHighlighter.user.js
@@ -1,35 +1,30 @@
 // ==UserScript==
-// @name          Canvas Syntax Highlighter
+// @name          Canvas Syntax Highlighter (Beta)
 // @description   "Replaces" the "HTML Editor" with the Ace Syntax Highlighter (https://ace.c9.io/)
 // @include       /^https?:\/\/[^\.]*\.([^\.]*\.)?instructure\.com\/.*$/
 // @exclude       /^https?:\/\/[^\.]*\.quiz-lti-iad-prod.instructure\.com\/.*$/
-// @version       1.6
-// @updateURL     https://raw.githubusercontent.com/cesbrandt/canvas-javascript-syntaxHighlighter/master/canvasSyntaxHighlighter.user.js
+// @version       2.0
+// @require       https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+//// @updateURL     https://raw.githubusercontent.com/cesbrandt/canvas-javascript-syntaxHighlighter/master/canvasSyntaxHighlighter.user.js
 // ==/UserScript==
 
 /**
  * Config
  */
-var editorLinksText = ['HTML Editor', 'Rich Content Editor'];   // These are the text of the Canvas editor links immediately above the editors. Replace only if your Canvas does not display in English.
-var toggleState = ['Disable', 'Enable'];                        // These are the prefixes to the toggle anchor. Update them for your personal langauge configuration preference.
-var toggleName = 'Syntax Highlighter';                          // This is the text for the toggle. Update it for your personal language preference.
-var editorWidth = '';    // If left blank, will default to 100%
-var editorHeight = '';   // If left blank, will default to 280px
+var editorLinksText = {   // These are the text of the Canvas editor links immediately above the editors. Replace only if your Canvas does not display in English.
+	old: ['HTML Editor', 'Rich Content Editor'],
+	new: ['Switch to raw html editor', 'Switch to rich text editor']
+};
+var toggleState = ['Disable', 'Enable'];   // These are the prefixes to the toggle anchor. Update them for your personal langauge configuration preference.
+var toggleName = 'Syntax Highlighter';     // This is the text for the toggle. Update it for your personal language preference.
+var editorWidth = 'calc(100% - 24px)';                 // If left blank, will default to 100%
+var editorHeight = '660px';                // If left blank, will default to 280px
 var opts = {
-	"indent_char": "\t",        // What character should be used to indent with (\t - tab; \s - space)
-	"indent_size": "1",         // How many times should that indentation character be used per level
-	"wrap_line_length": "80",   // At what character count should the line wrap to a new line? Note: This
-                                //    is soft value and should it occur in the middle of a string of characters
-                                //    the wrap will occur at the next word-break
-
-////////////////////////////////////
-////////////////////////////////////
-//                                //
-//  DO NOT EDIT BELOW THIS POINT  //
-//                                //
-////////////////////////////////////
-////////////////////////////////////
-
+	"indent_char": "\t",               // What character should be used to indent with (\t - tab; \s - space)
+	"indent_size": "1",                // How many times should that indentation character be used per level
+	"wrap_line_length": "80",          // At what character count should the line wrap to a new line? Note: This
+	                                   //    is soft value and should it occur in the middle of a string of characters
+	                                   //    the wrap will occur at the next word-break
 	"max_preserve_newlines": "0",
 	"preserve_newlines": true,
 	"keep_array_indentation": false,
@@ -45,11 +40,19 @@ var opts = {
 	"e4x": false
 };
 
+////////////////////////////////////
+////////////////////////////////////
+//                                //
+//  DO NOT EDIT BELOW THIS POINT  //
+//                                //
+////////////////////////////////////
+////////////////////////////////////
+
 /**
  * Don't run in frames
  */
 if(window.top !== window.self) {
-	return;
+	throw new Error(toggleName + ' will not load in frames.');
 }
 
 /**
@@ -67,24 +70,15 @@ var subviewID = (subview !== null && typeof leveledURL[6] !== 'undefined') ? lev
 var terview = (viewID !== null && typeof leveledURL[7] !== 'undefined') ? leveledURL[7].split('#')[0] : null;
 
 /**
- * @name              Function Extender
- * @description       Extends a function into subfunctions
- * @return function   Extended function
+ * Syntax Highlighter "class"
  */
-function extend(func, props) {
-	for(var prop in props) {
-		if(props.hasOwnProperty(prop)) {
-			func[prop] = props[prop];
-		}
-	}
-	return func;
-}
-
-var SH = extend(function() {
+var SH = extend(function(newEditor) {
+    SH.tinyMCE = newEditor ? 5 : 4;
 	SH.cookieName = server + '_Canvas_syntaxHighlighter';
 	SH.cookie = getCookie(SH.cookieName);
-	SH.rceEditor = '.ic-RichContentEditor > .mce-tinymce:first-of-type';
-	SH.htmlEditor = '.ic-RichContentEditor > textarea:first-of-type';
+	SH.editorContainer = newEditor ? '.canvas-rce__skins--root.rce-wrapper' : '.ic-RichContentEditor';
+	SH.rceEditor = SH.editorContainer + ' > .mce-tinymce:first-of-type';
+	SH.htmlEditor = SH.editorContainer + ' > textarea:first-of-type';
 	SH.enabled = SH.cookie !== '' ? JSON.parse(SH.cookie) : false;
 
 	return SH.init();
@@ -104,69 +98,104 @@ var SH = extend(function() {
 			});
 		});
 
-		// Update Ace Editor on View Switch
-		var switchClass;
-		switch(subview) {
-			case 'assignments':
-			case 'discussion_topics':
-			case null:
-				switchClass = $('#course_syllabus').length > 0 ? 'toggle_views_link' : 'rte_switch_views_link';
-				break;
-			case 'pages':
-				switchClass = 'switch_views';
-				break;
-			case 'quizzes':
-				switchClass = 'toggle_description_views_link';
-				break;
-		}
-		var i = 0;
-		var wait = setInterval(function() {
-			if($('.' + switchClass).length > 0) {
-				$('.' + switchClass).each(function() {
-					if(new RegExp(editorLinksText[0]).test($(this).text())) {
-						$(this).on('click', function() {
-							if(SH.enabled) {
-								SH.initAce();
-							}
-						});
-					} else if(new RegExp(editorLinksText[1]).test($(this).text())) {
-						$(this).on('click', function() {
-							if(SH.enabled) {
-								SH.endAce();
-							}
-						});
-					}
-				});
-
-				// Add to Editor Toggle Bar
-				$('.' + switchClass).last().after(' | ', $('<a />').attr({id: 'syntaxHighlighterToggle'}).css({cursor: 'pointer'}).html((SH.enabled ? toggleState[0] : toggleState[1]) + ' ' + toggleName).click(function(e) {
-					e.preventDefault();
-					SH.enabled = SH.enabled ? false : true;
-					setCookie(SH.cookieName, SH.enabled);
-					$(this).html((SH.enabled ? toggleState[0] : toggleState[1]) + ' ' + toggleName);
-					if(SH.enabled && $(SH.rceEditor).is(':hidden')) {
-						SH.initAce();
-					} else {
-						SH.endAce();
-					}
-				}));
-				clearInterval(wait);
-			} else if(i >= 30) {
-				console.log('Unable to identify switch links to load ' + toggleName);
-				clearInterval(wait);
-			}
-			i++;
-		}, 500);
-		return;
+		return SH.loadToggle();
 	},
+    loadToggle: function() {
+		console.log('Toggle');
+        if(SH.tinyMCE === 4) {
+            // Update Ace Editor on View Switch
+            var switchClass;
+            switch(subview) {
+                case 'assignments':
+                case 'discussion_topics':
+                case null:
+                    switchClass = $('#course_syllabus').length > 0 ? 'toggle_views_link' : 'rte_switch_views_link';
+                    break;
+                case 'pages':
+                    switchClass = 'switch_views';
+                    break;
+                case 'quizzes':
+                    switchClass = 'toggle_description_views_link';
+                    break;
+            }
+            var i = 0;
+            var wait = setInterval(function() {
+                if($('.' + switchClass).length > 0) {
+                    $('.' + switchClass).each(function() {
+                        if(new RegExp(editorLinksText.old[0]).test($(this).text())) {
+                            $(this).on('click', function() {
+                                if(SH.enabled) {
+                                    SH.initAce();
+                                }
+                            });
+                        } else if(new RegExp(editorLinksText.old[1]).test($(this).text())) {
+                            $(this).on('click', function() {
+                                if(SH.enabled) {
+                                    SH.endAce();
+                                }
+                            });
+                        }
+                    });
+
+                    // Add to Editor Toggle Bar
+                    $('.' + switchClass).last().after(' | ', $('<a />').attr({id: 'syntaxHighlighterToggle'}).css({cursor: 'pointer'}).html((SH.enabled ? toggleState[0] : toggleState[1]) + ' ' + toggleName).click(function(e) {
+                        e.preventDefault();
+                        SH.enabled = SH.enabled ? false : true;
+                        setCookie(SH.cookieName, SH.enabled);
+                        $(this).html((SH.enabled ? toggleState[0] : toggleState[1]) + ' ' + toggleName);
+                        if(SH.enabled && $(SH.rceEditor).is(':hidden')) {
+                            SH.initAce();
+                        } else {
+                            SH.endAce();
+                        }
+                    }));
+                    clearInterval(wait);
+                } else if(i >= 30) {
+                    console.log('Unable to identify switch links to load ' + toggleName);
+                    clearInterval(wait);
+                }
+                i++;
+            }, 500);
+        } else if(SH.tinyMCE === 5) {
+            var toggleEditor = function() {
+                console.log($('#' + SH.toggleID), $('#' + SH.toggleID).parent().parent());
+                if(new RegExp(editorLinksText.new[0]).test($(this).text())) {
+					if(SH.enabled) {
+						SH.initAce();
+					}
+                    console.log('Showing toggle');
+                    $('#' + SH.toggleID).parent().parent().show();
+                } else if(new RegExp(editorLinksText.new[1]).test($(this).text())) {
+                    SH.endAce();
+                    console.log('Hiding toggle');
+                    $('#' + SH.toggleID).parent().parent().hide();
+                }
+            };
+            SH.toggleID = toggleName.replace(/\s/g, '') + 'Toggle';
+            var toggle = $('<div />').addClass('ic-Form-control').css({display: 'inline-block', margin: 0, verticalAlign: 'middle'}).html('<label class="ic-Super-toggle--ui-switch" for="' + SH.toggleID + '"><span class="screenreader-only">' + (SH.enabled ? toggleState[0] : toggleState[1]) + ' ' + toggleName + '</span><input type="checkbox" id="' + SH.toggleID + '" class="ic-Super-toggle__input"' + (SH.enabled ? ' checked="checked"' : '') + '><div class="ic-Super-toggle__container" aria-hidden="true" data-checked="' + SH.toggleID + ': true" data-unchecked="' + SH.toggleID + ': false"><div class="ic-Super-toggle__switch"></div></div></label>');
+            toggle.find('#' + SH.toggleID).on('change', function() {
+				SH.enabled = SH.enabled ? false : true;
+				setCookie(SH.cookieName, SH.enabled);
+                if(SH.enabled && $(SH.editorContainer + ' > .tox.tox-tinymce').is(':hidden')) {
+                    SH.initAce();
+                } else {
+                    SH.endAce();
+                }
+            });
+            $('button[title="' + editorLinksText.new[$('button[title="' + editorLinksText.new[0] + '"]').length > 0 ? 0 : 1] + '"]').on('click', toggleEditor).after($('button[title="' + editorLinksText.new[0] + '"]').length > 0 ? toggle.hide() : toggle);
+        }
+        return;
+    },
 	initAce: function() {
 		setTimeout(function() {
 			// Build Ace Editor
-			$('.ic-RichContentEditor').append($('<pre />').attr({'id': SH.cookieName}));
+			$(SH.editorContainer + ' > textarea').after($('<pre />').attr({'id': SH.cookieName}));
 
 			// Auto-Indent
 			$(SH.htmlEditor).addClass('hide');
-			$('#editor_tabs').addClass('hide');
+			if(SH.tinyMCE === 4) {
+				$('#right-side-wrapper').hide();
+			}
 			var source = $(SH.htmlEditor).val();
 			$('#' + SH.cookieName).text(html_beautify(source, opts));
 
@@ -189,9 +218,29 @@ var SH = extend(function() {
 	endAce: function() {
 		$('#' + SH.cookieName).remove();
 		$(SH.htmlEditor).removeClass('hide');
-		$('#editor_tabs').removeClass('hide');
+		if(SH.tinyMCE === 4) {
+			$('#right-side-wrapper').show();
+		}
 	}
 });
+
+/*************
+ * Functions *
+ *************/
+
+/**
+ * @name              Function Extender
+ * @description       Extends a function into subfunctions
+ * @return function   Extended function
+ */
+function extend(func, props) {
+	for(var prop in props) {
+		if(props.hasOwnProperty(prop)) {
+			func[prop] = props[prop];
+		}
+	}
+	return func;
+}
 
 /**
  * @name            Get Cookie
@@ -228,8 +277,27 @@ function setCookie(name, value, expire) {
 	return;
 }
 
-(function() {
+window.onload = function() {
+	// Patch for jQuery detection, seems page-loaded is sometimes blocked in userscripts
+	this.$ = this.jQuery = jQuery.noConflict(true);
+
 	if($.inArray(view, ['login', 'api']) === -1 && (($.inArray(view, ['courses', 'users']) > -1 && subview != 'files') || (view == 'accounts' && subview != 'settings'))) {
-		SH();
+		/**
+		 * Wait for editor to load
+		 */
+		var i = 0;
+		var wait = setInterval(function() {
+			if(typeof tinyMCE !== "undefined" && tinyMCE.editors.length > 0) {
+				if(tinyMCE.majorVersion == 4) {
+					SH();
+				} else if(tinyMCE.majorVersion == 5) {
+					SH(true);
+				}
+				clearInterval(wait);
+			}
+			if(i++ >= 30) {
+				clearInterval(wait);
+			}
+		}, 500);
 	}
-})();
+};
