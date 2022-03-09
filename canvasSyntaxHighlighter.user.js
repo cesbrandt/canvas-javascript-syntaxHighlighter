ba232b1802a4eaf7bbe183797e9c823206e83f80
Christopher E <cesbrandt@users.noreply.github.com>
2022-03-09 09:22:36 -0500

    Update canvasSyntaxHighlighter.user.js
    - Removed jQuery
    - Updated Ace to v1.4.14
    - Updated JS-Beautify to v1.14.0

--- canvasSyntaxHighlighter.user.js
+++ canvasSyntaxHighlighter.user.js
@@ -3,8 +3,7 @@
 // @description   "Replaces" the "HTML Editor" with the Ace Syntax Highlighter (https://ace.c9.io/)
 // @include       /^https?:\/\/[^\.]*\.([^\.]*\.)?instructure\.com\/.*$/
 // @exclude       /^https?:\/\/[^\.]*\.quiz-lti-iad-prod.instructure\.com\/.*$/
-// @version       3.1
-// @require       https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @version       3.2
 // @updateURL     https://raw.githubusercontent.com/cesbrandt/canvas-javascript-syntaxHighlighter/master/canvasSyntaxHighlighter.user.js
 // ==/UserScript==
 
@@ -12,8 +11,10 @@
  * Config
  */
 var editorLinksText = {   // These are the text of the Canvas editor links immediately above the editors. Replace only if your Canvas does not display in English.
-	old: ['HTML Editor', 'Rich Content Editor'],
-	new: ['Switch to raw html editor', 'Switch to rich text editor']
+	rce: 'Click or shift-click for the html editor.',
+	html: 'Switch to the rich text editor',
+	pretty: 'Raw HTML Editor',
+	raw: 'Pretty HTML Editor'
 };
 var fontSize = '';                         // This determines the size of the text in the editor, will default to 12px
 var toggleState = ['Disable', 'Enable'];   // These are the prefixes to the toggle anchor. Update them for your personal langauge configuration preference.
@@ -70,114 +71,28 @@ var subview = (viewID !== null && typeof leveledURL[5] !== 'undefined') ? levele
 var subviewID = (subview !== null && typeof leveledURL[6] !== 'undefined') ? leveledURL[6].split('#')[0] : null;
 var terview = (viewID !== null && typeof leveledURL[7] !== 'undefined') ? leveledURL[7].split('#')[0] : null;
 
-/**
- * Syntax Highlighter "class"
- */
-var SH = extend(function() {
-	SH.cookieName = server + '_Canvas_syntaxHighlighter';
-	SH.cookie = getCookie(SH.cookieName);
-	SH.editorContainer = '.canvas-rce__skins--root.rce-wrapper';
-	SH.rceEditor = $(SH.editorContainer + ' .tox-tinymce').first();
-	SH.htmlEditor = $(SH.editorContainer + ' textarea').first();
-	SH.htmlEditorToggle = $(SH.editorContainer + ' > [data-testid="RCEStatusBar"] > [role="toolbar"] [title="' + editorLinksText.new[0] + '"]');
-	SH.enabled = SH.cookie !== '' ? JSON.parse(SH.cookie) : false;
-
-	return SH.init();
-}, {
-	init: function() {
-		console.log('SH: Initiating');
-
-		// Load Ace Editor
-		$.getScript('https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ace.js', function(data, status, xhr) {
-			$.getScript('https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.13.4/beautify.min.js', function(data, status, xhr) {
-				$.getScript('https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.13.4/beautify-html.min.js', function(data, status, xhr) {
-					$.getScript('https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.13.4/beautify-css.min.js', function(data, status, xhr) {
-						$('head').append($('<style />').text('#' + SH.cookieName + ' { width: ' + (typeof editorWidth !== 'undefined' && editorWidth !== '' ? editorWidth : '100%') + '; height: ' + (typeof editorHeight !== 'undefined' && editorHeight !== '' ? editorHeight : '280px') + '; z-index: 100;' + (fontSize !== '' ? ' font-size: ' + fontSize + ';' : '') + ' } #syntaxHighlighterToggle { display: inline !important; }'));
-						if(SH.enabled && SH.rceEditor.is(':hidden')) {
-							SH.initAce();
-						}
-					});
-				});
-			});
-		});
-
-		return SH.loadToggle();
-	},
-	loadToggle: function() {
-		var toggleEditor = function() {
-			$('#' + SH.toggleID).parent().parent().remove();
-			SH.endAce();
-
-			console.log(SH.rceEditor, SH.rceEditor.css('display'));
-			if(SH.rceEditor.css('display') != 'none') {
-				var toggle = $('<div />').addClass('ic-Form-control').css({display: 'inline-block', margin: 0, verticalAlign: 'middle'}).html('<label class="ic-Super-toggle--ui-switch" for="' + SH.toggleID + '"><span class="screenreader-only">' + (SH.enabled ? toggleState[0] : toggleState[1]) + ' ' + toggleName + '</span><input type="checkbox" id="' + SH.toggleID + '" class="ic-Super-toggle__input"' + (SH.enabled ? ' checked="checked"' : '') + '><div class="ic-Super-toggle__container" aria-hidden="true" data-checked="' + SH.toggleID + ': true" data-unchecked="' + SH.toggleID + ': false"><div class="ic-Super-toggle__switch"></div></div></label>');
-				toggle.find('#' + SH.toggleID).on('change', function() {
-					SH.enabled = SH.enabled ? false : true;
-					setCookie(SH.cookieName, SH.enabled);
-					if(SH.enabled) {
-						$(this).parent().find('.screenreader-only').text(toggleState[0] + ' ' + toggleName);
-						SH.initAce();
-					} else {
-						$(this).parent().find('.screenreader-only').text(toggleState[1] + ' ' + toggleName);
-						SH.endAce();
-					}
-				});
-
-				SH.htmlEditorToggle.before(toggle);
-				if(SH.enabled) {
-					SH.initAce();
-				}
-			}
-		};
-		SH.toggleID = toggleName.replace(/\s/g, '') + 'Toggle';
-		console.log(SH.htmlEditorToggle);
-		SH.htmlEditorToggle.on('click', toggleEditor);
-		return;
-	},
-	initAce: function() {
-		console.log('SH: Loading Ace');
-
-		setTimeout(function() {
-			// Build Ace Editor
-			SH.htmlEditor.after($('<pre />').attr({'id': SH.cookieName}));
-
-			// Auto-Indent
-			SH.htmlEditor.addClass('hide');
-			var source = SH.htmlEditor.val();
-			$('#' + SH.cookieName).text(html_beautify(source, opts));
-
-			// Initialize Ace Editor
-			var editor = ace.edit(SH.cookieName, {
-				mode: 'ace/mode/html',
-				selectionStyle: 'text',
-				theme: 'ace/theme/sqlserver'
-			});
-
-			// Update HTML Editor
-			$('#' + SH.cookieName).on('focusout', function() {
-				SH.htmlEditor.val(editor.getValue().replace(/^\s+</gm, '<').replace(/(\n|\r)([^\S\n\r]{4}|\t)+/gm, ' ')).trigger('change');
-			});
-
-			// Set focus on Ace Editor
-			editor.focus();
-		}, 100);
-	},
-	endAce: function() {
-		$('#' + SH.cookieName).remove();
-		SH.htmlEditor.removeClass('hide');
-	}
-});
-
 /*************
  * Functions *
  *************/
+
+/**
+ * @name          Trigger Event
+ * @description   Triggers a specified event listener
+ * @return
+ */
+let triggerEvent = (eventName, element) => {
+	var event = document.createEvent('HTMLEvents');
+	event.initEvent(eventName, false, true);
+	element.dispatchEvent(event);
+	return;
+};
 
 /**
  * @name              Function Extender
  * @description       Extends a function into subfunctions
  * @return function   Extended function
  */
-function extend(func, props) {
+let extend = (func, props) => {
 	for(var prop in props) {
 		if(props.hasOwnProperty(prop)) {
 			func[prop] = props[prop];
@@ -187,11 +102,44 @@ function extend(func, props) {
 }
 
 /**
+ * @name            Load Scripts
+ * @description     Loads a script file to a page
+ * @return
+ */
+let loadScripts = (urls, callback) => {
+	var promises = [];
+	switch(typeof urls) {
+		case 'string':
+			urls = [urls];
+			break;
+		case 'object':
+			if(Array.isArray(urls)) {
+				break;
+			}
+			return;
+		default:
+			return;
+	}
+	urls.forEach((url, i) => {
+		promises[i] = new Promise((resolve, reject) => {
+			var body = document.body || document.querySelector('body') || document.getElementsByTagName('body')[0];
+			var script = document.createElement('script');
+			script.addEventListener('load', resolve);
+			script.addEventListener('error', reject);
+			script.async = true;
+			script.src = url;
+			body.appendChild(script);
+		});
+	});
+	Promise.all(promises).then(callback);
+};
+
+/**
  * @name            Get Cookie
  * @description     Lookup a cookie by name
  * @return string   Value of cookie
  */
-function getCookie(name) {
+let getCookie = (name) => {
 	name += '=';
 	var cookies = decodeURIComponent(document.cookie).split(';');
 	for(var i = 0; i < cookies.length; i++) {
@@ -210,7 +158,7 @@ function getCookie(name) {
  * @description     Set a cookie by name
  * @return undefined
  */
-function setCookie(name, value, expire) {
+let setCookie = (name, value, expire) => {
 	var expires = '';
 	if(!isNaN(expire)) {
 		var expireDate = new Date();
@@ -221,16 +169,145 @@ function setCookie(name, value, expire) {
 	return;
 }
 
-window.onload = function() {
-	// Patch for jQuery detection, seems page-loaded is sometimes blocked in userscripts
-	this.$ = this.jQuery = jQuery.noConflict(true);
+/**
+ * Syntax Highlighter "class"
+ */
+let SH = extend(() => {
+	SH.cookieName = server + '_Canvas_syntaxHighlighter';
+	SH.cookie = getCookie(SH.cookieName);
+	SH.editorContainer = '.canvas-rce__skins--root.rce-wrapper';
+	SH.rceEditor = document.querySelector(SH.editorContainer + ' .tox-tinymce');
+	SH.prettyEditor = SH.editorContainer + ' .CodeMirror';
+	SH.prettyEditorToggle = SH.editorContainer + ' > [data-testid="RCEStatusBar"] [data-btn-id="rce-editormessage-btn"]';
+	SH.htmlEditor = document.querySelector(SH.editorContainer + ' textarea');
+	SH.htmlEditorToggle = document.querySelector(SH.editorContainer + ' > [data-testid="RCEStatusBar"] > [role="toolbar"] [title="' + editorLinksText.rce + '"]');
+	SH.enabled = SH.cookie !== '' ? JSON.parse(SH.cookie) : false;
 
-	if($.inArray(view, ['login', 'api']) === -1 && (($.inArray(view, ['courses', 'users']) > -1 && subview != 'files') || (view == 'accounts' && subview != 'settings'))) {
+	return SH.init();
+}, {
+	init: () => {
+		console.log('SH: Initiating');
+
+		// Load Ace Editor
+		loadScripts([
+			'https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.14/ace.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.14.0/beautify.min.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.14.0/beautify-html.min.js',
+			'https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.14.0/beautify-css.min.js'
+		], () => {
+			var head = document.head || document.querySelector('head') || document.getElementsByTagName('head')[0];
+			var css = document.createElement('style');
+			css.innerHTML = '#' + SH.cookieName + ' { width: ' + (typeof editorWidth !== 'undefined' && editorWidth !== '' ? editorWidth : '100%') + '; height: ' + (typeof editorHeight !== 'undefined' && editorHeight !== '' ? editorHeight : '280px') + '; z-index: 100;' + (fontSize !== '' ? ' font-size: ' + fontSize + ';' : '') + ' } #syntaxHighlighterToggle { display: inline !important; }';
+			head.appendChild(css);
+			if(SH.enabled && SH.rceEditor.style.display == 'none' && document.querySelector(SH.prettyEditorToggle).innerText != editorLinksText.pretty) {
+				SH.initAce();
+			}
+
+			return SH.loadToggle();
+		});
+	},
+	loadToggle: () => {
+		var toggleEditor = () => {
+			if(document.contains(document.querySelector('#' + SH.toggleID))) {
+				document.querySelector('#' + SH.toggleID).parentNode.parentNode.remove();
+			}
+			SH.endAce();
+
+			if(SH.rceEditor.style.display != 'none') {
+				var toggle = document.createElement('div');
+				toggle.classList.add('ic-Form-control');
+				toggle.style.cssText = "display: inline-block; margin: 0; vertical-align: middle;";
+				toggle.innerHTML = '<label class="ic-Super-toggle--ui-switch" for="' + SH.toggleID + '"><span class="screenreader-only">' + (SH.enabled ? toggleState[0] : toggleState[1]) + ' ' + toggleName + '</span><input type="checkbox" id="' + SH.toggleID + '" class="ic-Super-toggle__input"' + (SH.enabled ? ' checked="checked"' : '') + ' /><div class="ic-Super-toggle__container" aria-hidden="true" data-checked="' + SH.toggleID + ': true" data-unchecked="' + SH.toggleID + ': false"><div class="ic-Super-toggle__switch"></div></div></label>';
+				toggle.querySelector('#' + SH.toggleID).addEventListener('change', (ele) => {
+					SH.enabled = SH.enabled ? false : true;
+					setCookie(SH.cookieName, SH.enabled);
+					if(SH.enabled) {
+						ele.currentTarget.parentNode.querySelector('.screenreader-only').innerText = toggleState[0] + ' ' + toggleName;
+						SH.initAce();
+					} else {
+						ele.currentTarget.parentNode.querySelector('.screenreader-only').innerText = toggleState[1] + ' ' + toggleName;
+						SH.endAce();
+					}
+				});
+
+				SH.htmlEditorToggle.parentNode.insertBefore(toggle, SH.htmlEditorToggle);
+				if(SH.enabled) {
+					SH.initAce();
+				}
+			}
+
+			var wait = setInterval(() => {
+				if(document.contains(document.querySelector(SH.prettyEditorToggle))) {
+					clearInterval(wait);
+
+					document.querySelector(SH.prettyEditorToggle).addEventListener('click', () => {
+						if(SH.enabled && document.querySelector(SH.prettyEditorToggle).innerText != editorLinksText.raw) {
+							SH.endAce();
+							SH.initAce();
+						}
+					});
+				}
+			}, 250);
+		};
+		SH.toggleID = toggleName.replace(/\s/g, '') + 'Toggle';
+		SH.htmlEditorToggle.addEventListener('click', toggleEditor);
+
+		return;
+	},
+	initAce: () => {
+		console.log('SH: Loading Ace');
+
+		setTimeout(() => {
+			// Build Ace Editor
+			(new Promise((resolve, reject) => {
+				var pre = document.createElement('pre');
+				pre.setAttribute('id', SH.cookieName);
+				SH.htmlEditor.parentNode.insertBefore(pre, SH.htmlEditor.nextSibling);
+
+				return resolve();
+			})).then(() => {
+				// Hide the HTML Editor
+				SH.htmlEditor.classList.add('hide');
+
+				// Initialize Ace Editor
+				var editor = ace.edit(SH.cookieName, {
+					mode: 'ace/mode/html',
+					selectionStyle: 'text',
+					theme: 'ace/theme/sqlserver'
+				});
+
+				// Auto-Indent
+				var source = SH.htmlEditor.value;
+				editor.setValue(html_beautify(source, opts), -1);
+
+				// Update HTML Editor
+				document.querySelector('#' + SH.cookieName).addEventListener('focusout', () => {
+					SH.htmlEditor.value = editor.getValue().replace(/^\s+</gm, '<').replace(/(\n|\r)([^\S\n\r]{4}|\t)+/gm, ' ');
+					triggerEvent('change', SH.htmlEditor);
+				});
+
+				// Set focus on Ace Editor
+				editor.focus();
+			});
+		}, 100);
+	},
+	endAce: () => {
+		console.log('SH: Unloading Ace');
+
+		if(document.contains(document.querySelector('#' + SH.cookieName))) {
+			document.querySelector('#' + SH.cookieName).remove();
+		}
+		SH.htmlEditor.classList.remove('hide');
+	}
+});
+
+window.onload = () => {
+	if(!['login', 'api'].includes(view) && ((['courses', 'users'].includes(view) && subview != 'files') || (view == 'accounts' && subview != 'settings'))) {
 		/**
 		 * Wait for editor to load
 		 */
 		var i = 0;
-		var wait = setInterval(function() {
+		var wait = setInterval(() => {
 			if(typeof tinyMCE !== "undefined" && tinyMCE.editors.length > 0) {
 				SH();
 				clearInterval(wait);
